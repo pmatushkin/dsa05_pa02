c5ed44f45ec918d610f120a23fdd78efd29b423b
pmatushkin <pmatushkin@gmail.com>
2017-02-17 20:58:49 -0800

    fix: Rename variables for easier reading

--- src/energy_values/EnergyValues.java
+++ src/energy_values/EnergyValues.java
@@ -13,13 +13,13 @@ class Equation {
 }
 
 class Position {
-    Position(int column, int raw) {
+    Position(int column, int row) {
         this.column = column;
-        this.raw = raw;
+        this.row = row;
     }
 
     int column;
-    int raw;
+    int row;
 }
 
 class EnergyValues {
@@ -29,10 +29,10 @@ class EnergyValues {
 
         double a[][] = new double[size][size];
         double b[] = new double[size];
-        for (int raw = 0; raw < size; ++raw) {
+        for (int row = 0; row < size; ++row) {
             for (int column = 0; column < size; ++column)
-                a[raw][column] = scanner.nextInt();
-            b[raw] = scanner.nextInt();
+                a[row][column] = scanner.nextInt();
+            b[row] = scanner.nextInt();
         }
         return new Equation(a, b);
     }
@@ -52,32 +52,32 @@ class EnergyValues {
         return new Position(column, free);
     }
 
-    static void SwapLines(double a[][], double b[], boolean used_raws[], Position pivot_element) {
+    static void SwapLines(double a[][], double b[], boolean used_rows[], Position pivot_element) {
         int size = a.length;
 
         for (int column = 0; column < size; ++column) {
             double tmpa = a[pivot_element.column][column];
-            a[pivot_element.column][column] = a[pivot_element.raw][column];
-            a[pivot_element.raw][column] = tmpa;
+            a[pivot_element.column][column] = a[pivot_element.row][column];
+            a[pivot_element.row][column] = tmpa;
         }
 
         double tmpb = b[pivot_element.column];
-        b[pivot_element.column] = b[pivot_element.raw];
-        b[pivot_element.raw] = tmpb;
+        b[pivot_element.column] = b[pivot_element.row];
+        b[pivot_element.row] = tmpb;
 
-        boolean tmpu = used_raws[pivot_element.column];
-        used_raws[pivot_element.column] = used_raws[pivot_element.raw];
-        used_raws[pivot_element.raw] = tmpu;
+        boolean tmpu = used_rows[pivot_element.column];
+        used_rows[pivot_element.column] = used_rows[pivot_element.row];
+        used_rows[pivot_element.row] = tmpu;
 
-        pivot_element.raw = pivot_element.column;
+        pivot_element.row = pivot_element.column;
     }
 
     static void ProcessPivotElement(double a[][], double b[], Position pivot_element) {
         // Write your code here
     }
 
-    static void MarkPivotElementUsed(Position pivot_element, boolean used_raws[], boolean used_columns[]) {
-        used_raws[pivot_element.raw] = true;
+    static void MarkPivotElementUsed(Position pivot_element, boolean used_rows[], boolean used_columns[]) {
+        used_rows[pivot_element.row] = true;
         used_columns[pivot_element.column] = true;
     }
 
@@ -87,12 +87,12 @@ class EnergyValues {
         int size = a.length;
 
         boolean[] used_columns = new boolean[size];
-        boolean[] used_raws = new boolean[size];
+        boolean[] used_rows = new boolean[size];
         for (int step = 0; step < size; ++step) {
             Position pivot_element = SelectPivotElement(a, step);
-            SwapLines(a, b, used_raws, pivot_element);
+            SwapLines(a, b, used_rows, pivot_element);
             ProcessPivotElement(a, b, pivot_element);
-            MarkPivotElementUsed(pivot_element, used_raws, used_columns);
+            MarkPivotElementUsed(pivot_element, used_rows, used_columns);
         }
 
         return b;
@@ -100,8 +100,8 @@ class EnergyValues {
 
     static void PrintColumn(double column[]) {
         int size = column.length;
-        for (int raw = 0; raw < size; ++raw)
-            System.out.printf("%.20f\n", column[raw]);
+        for (int row = 0; row < size; ++row)
+            System.out.printf("%.20f\n", column[row]);
     }
 
     public static void main(String[] args) throws IOException {
